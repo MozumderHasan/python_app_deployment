e8b96fd997b23dc04480500ac7ed8358ae169cce
MozumderHasan <md.mozumder.hasan@gmail.com>
2023-11-18 11:37:13 +0600

    add docker file and docker ignore file and some port and command changes for docker image build

--- python-micro-service/main.py
+++ python-micro-service/main.py
@@ -1,8 +1,10 @@
+import socket
 from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
-ip = '192.168.0.110'
+#ip = '192.168.0.110'
+ip = socket.gethostbyname(socket.gethostname())
 # Render the homepage with buttons
 @app.route('/')
 def index():
@@ -26,4 +28,4 @@ def redirect_9000():
 if __name__ == '__main__':
     # If running with Gunicorn, you can start the app using Gunicorn like this:
     # gunicorn main:app -b 0.0.0.0:8080
-    app.run(port=8010)
+    app.run(port=5000)
--- python-micro-service/shellmain.sh
+++ python-micro-service/shellmain.sh
@@ -7,4 +7,16 @@
 #gnome-terminal
 
 # Run requirements and app.py in the new terminal window.
-source vFlask/bin/activate && pip3 install -r requirements.txt && python3 main.py
+source vFlask/bin/activate && pip3 install -r requirements.txt && python3 main.py &
+
+cd document_app
+
+gnome-terminal -e "./shellapp3.sh" &
+# Go to the third directory.
+cd ..
+cd music_app
+gnome-terminal -e "./shellapp2.sh" &
+# Go to the fourth directory.
+cd ..
+cd post_app
+gnome-terminal -e "./shellapp1.sh"
